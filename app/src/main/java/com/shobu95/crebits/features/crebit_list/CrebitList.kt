da3367c3bef5b34185779c1545c32a9ee5fb9c5e
shobu95 <shoaib.ahmedsk95@gmail.com>
2021-09-05 19:09:00 +0500

    added FAB to CrebitList, navigated to Add Crebit on click

--- app/src/main/java/com/shobu95/crebits/features/crebit_list/CrebitList.kt
+++ app/src/main/java/com/shobu95/crebits/features/crebit_list/CrebitList.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.shobu95.crebits.R
+import com.shobu95.crebits.databinding.ActivityMainBinding
+import com.shobu95.crebits.databinding.FragmentCrebitListBinding
 
 class CrebitList : Fragment() {
 
+    private lateinit var binding: FragmentCrebitListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crebit_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_crebit_list, container, false)
+
+        binding.fabAddCrebit.setOnClickListener { v: View ->
+            v.findNavController().navigate(CrebitListDirections.actionCrebitListToAddEditCrebit())
+        }
+
+        return binding.root;
     }
 
 }
