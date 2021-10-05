7bc887f8fe91fec7fb3b6c5dc85f59148b9bef49
Siddharth Singh <siddhartsingh2002@gmail.com>
2021-10-05 20:33:27 +0530

    New UI

--- app/src/main/java/com/shobu95/crebits/screens/SplashScreenActivity.java
+++ app/src/main/java/com/shobu95/crebits/screens/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        int colorCodeDark = Color.parseColor("#38c172");
+        int colorCodeDark = Color.parseColor("#666AD1");
         window.setStatusBarColor(colorCodeDark);
 
         final Handler handler = new Handler(Looper.getMainLooper());
