7dcc2fe78eab15f9b526fcf9f4e9569b420ac99c
AliAzaz <ali.azaz.alam@hotmail.com>
2021-10-26 03:28:44 +0500

    Fixed issue #02, updated SplashScreenActivity and some minor modifications

--- app/src/main/java/com/shobu95/crebits/SplashScreenActivity.kt
+++ app/src/main/java/com/shobu95/crebits/SplashScreenActivity.kt
@@ -1,39 +1,32 @@
-package com.shobu95.crebits;
+package com.shobu95.crebits
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.Window;
-import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shobu95.crebits.R
+import android.view.WindowManager
+import android.os.Looper
+import android.content.Intent
+import android.graphics.Color
+import android.os.Handler
+import com.shobu95.crebits.screens.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-import androidx.appcompat.app.AppCompatActivity;
+class SplashScreenActivity : AppCompatActivity() {
 
-import com.shobu95.crebits.screens.MainActivity;
+    private val TIME_OUT: Long = 3000
 
-public class SplashScreenActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-
-        getSupportActionBar().hide();
-
-        Window window = getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        int colorCodeDark = Color.parseColor("#001970");
-        window.setStatusBarColor(colorCodeDark);
-
-        final Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        }, 3000);
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(TIME_OUT)
+            finish()
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        }
 
     }
 }
