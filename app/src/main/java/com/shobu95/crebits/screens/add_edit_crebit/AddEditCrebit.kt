f858b1970b6786e859248a57d65e3692bec91717
AliAzaz <ali.azaz.alam@hotmail.com>
2021-10-30 00:49:25 +0500

    I've resolved #1 as mentioned in comment. Secondly, I've also updated fragment_add_edit_crebit.xml layout

--- app/src/main/java/com/shobu95/crebits/screens/add_edit_crebit/AddEditCrebit.kt
+++ app/src/main/java/com/shobu95/crebits/screens/add_edit_crebit/AddEditCrebit.kt
@@ -27,7 +27,7 @@ class AddEditCrebit : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(
             inflater,
